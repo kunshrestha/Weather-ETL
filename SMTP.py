a644f3559ef7024c13445fbec08397d9068ea6f6
kunshrestha <kunshrestha@hotmail.com>
2022-09-22 18:27:36 +1000

    Update SMTP.py
    Added comments to describe the function.
    If trying to send email fails, instead of printing error to standard output, pass is used.

--- SMTP.py
+++ SMTP.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[6]:
+
 
 import smtplib
 from os.path import basename
@@ -12,6 +13,24 @@ from email.utils import COMMASPACE, formatdate
 
 def send_mail(send_from, send_to, subject, body, files=[],
               server="127.0.0.1", port=587, user="", password=""):
+    # This function sends email with or without attachment.
+    # Desable multi level authentication in email server
+    # for it to work.
+    # Currently only csv files are supported to send.
+    
+    # Parameters
+    # send_from: str, sender's email address
+    # send_to: str or list or str, receiver's address(es)
+    # subject: str, subject of email
+    # body: str, plain email body
+    # files: str or list of str, file path(s) to send as attachment, default []
+    # server: str, smtp server name, default 127.0.0.1
+    # port: int, smtp port, default 587,
+    # user: str, usually sender's email address
+    # password: str, email password
+
+    # Returns nothing
+    
     if not isinstance(send_to, list):
         send_to = [send_to]
     if not isinstance(files, list):
@@ -32,6 +51,7 @@ def send_mail(send_from, send_to, subject, body, files=[],
                 _subtype="csv",
                 Name=basename(f)
             )
+        # After the file is closed
         part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
         msg.attach(part)
         
@@ -43,9 +63,6 @@ def send_mail(send_from, send_to, subject, body, files=[],
         smtp.login(user, password)
         smtp.sendmail(send_from, send_to, msg.as_string())
     except Exception as e:
-        print(e)
+        pass
     finally:
         smtp.quit()
-
-
-
